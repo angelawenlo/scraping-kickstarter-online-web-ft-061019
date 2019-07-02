819a2508721d922582cf056b686da822e3eb22ff
Angela Lo <angelawenlo@gmail.com>
2019-07-02 14:19:59 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,7 +12,7 @@ def create_project_hash
   # title: project.css("h2.bbcard_name strong a").text
   # image_link: project.css("div.project-thumbnail a img").attribute("src").value
   # description: project.css("p.bbcard_blurb").text
-  # location:  project.css("ul.project-meta").text
+  # location: project.css("ul.project-meta").text
 
   binding.pry
 end
